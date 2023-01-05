04738e1f1417b7ac2ea61749acaf3a6da0a85d88
Aruuk <skybanychnek@gmail.com>
2023-01-05 11:39:32 +0600

    Initial commit

---
+++ src/main/java/com/example/task_tracker_entity_test/TaskTrackerEntityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_tracker_entity_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerEntityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerEntityTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task_tracker_entity_test/TaskTrackerEntityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_tracker_entity_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerEntityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
